0baf59d3925816c409a7272ab2522091c37ac019
Diego <diego2000avelar@gmail.com>
2021-01-03 16:44:24 -0600

    implementacion del guard admin para proteger rutas admin

--- src/app/@core/guards/admin.guard.ts
+++ src/app/@core/guards/admin.guard.ts
@@ -1,14 +1,43 @@
+import { AuthService } from '@core/services/auth.service';
 import { Injectable } from '@angular/core';
-import { CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-
+import {
+  CanActivateChild,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import jwtDecode from 'jwt-decode';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminGuard implements CanActivateChild {
+  constructor(private auth: AuthService, private router: Router) {}
   canActivateChild(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-    return true;
+    state: RouterStateSnapshot
+  ): boolean {
+    // comprobar existencia del token
+    if (this.auth.getSession() != null) {
+      const dataDecode = this.decodeToken();
+      // comprobar que el token no esta caducado
+      if (dataDecode.exp < new Date().getTime() / 1000) {
+        return this.redirect();
+      }
+      // comprobar si el usuario es ADMIN
+      if (dataDecode.user.role === 'ADMIN') {
+        return true;
+      }
+      return this.redirect();
+    }
+  }
+
+  redirect() {
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+  decodeToken(): any {
+    return jwtDecode(this.auth.getSession().token);
   }
 }
