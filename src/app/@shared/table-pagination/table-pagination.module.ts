b073dd29bc4c70635083e3033fa4433f6938a56d
Diego <diego2000avelar@gmail.com>
2021-02-13 09:19:15 -0600

    modulo paginacion creado

---
+++ src/app/@shared/table-pagination/table-pagination.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TablePaginationComponent } from './table-pagination.component';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [TablePaginationComponent],
+  imports: [
+    CommonModule,
+    NgbPaginationModule
+  ],
+  exports: [TablePaginationComponent]
+})
+export class TablePaginationModule { }
