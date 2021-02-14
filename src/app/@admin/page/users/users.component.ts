f913dcbe2d5901aecd00d78317179cd2c0ac94ca
Diego <diego2000avelar@gmail.com>
2021-02-14 13:00:04 -0600

    definiendo propiedades

--- src/app/@admin/page/users/users.component.ts
+++ src/app/@admin/page/users/users.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { IResultData } from './../../../@core/interfaces/result-data';
+import { USERS_LIST_QUERY } from './../../../@graphql/operations/query/user';
+import { Component, Input, OnInit } from '@angular/core';
+import {DocumentNode} from "graphql"
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
-
+  query : DocumentNode = USERS_LIST_QUERY;
+  context : object;
+  itemsPage: number;
+  include: boolean;
+  resultData: IResultData
   ngOnInit(): void {
   }
 
