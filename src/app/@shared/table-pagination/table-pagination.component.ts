b06b51b7816c69a3ef51f2c097ba1f86bcbbbcec
Diego <diego2000avelar@gmail.com>
2021-02-14 13:45:12 -0600

    cambios y mostrando info en paginator

--- src/app/@shared/table-pagination/table-pagination.component.ts
+++ src/app/@shared/table-pagination/table-pagination.component.ts
@@ -1,47 +1,52 @@
+import { map } from 'rxjs/internal/operators/map';
 import { IResultData, IInfoPage } from './../../@core/interfaces/result-data';
-import { USERS_LIST_QUERY } from './../../@graphql/operations/query/user';
 import { TablePaginationService } from './table-pagination.service';
 import { DocumentNode } from 'graphql';
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/observable';
 
 @Component({
   selector: 'app-table-pagination',
   templateUrl: './table-pagination.component.html',
-  styleUrls: ['./table-pagination.component.scss']
+  styleUrls: ['./table-pagination.component.scss'],
 })
 export class TablePaginationComponent implements OnInit {
-   @Input() query : DocumentNode = USERS_LIST_QUERY;
-   @Input() context : object;
-   @Input() itemsPage = 20;
-   @Input() resultData: IResultData;
-   @Input() include = true;
-   infoPage: IInfoPage;
-  constructor(private service: TablePaginationService) { }
+  @Input() query: DocumentNode;
+  @Input() context: object;
+  @Input() itemsPage = 20;
+  @Input() resultData: IResultData;
+  @Input() include = true;
+  infoPage: IInfoPage;
+  data$: Observable<any>
+  constructor(private service: TablePaginationService) {}
 
   ngOnInit(): void {
-    if(this.query === undefined){
-       throw new Error("Query is undefined")
+    if (this.query === undefined) {
+      throw new Error('Query is undefined');
     }
-    if(this.resultData === undefined){
-      throw new Error("resultData is undefined")
-   }
+    if (this.resultData === undefined) {
+      throw new Error('resultData is undefined');
+    }
     this.infoPage = {
       page: 1,
       pages: 1,
       itemsPage: this.itemsPage,
-      total: 1
-    }
+      total: 1,
+    };
     this.loadData();
   }
-   loadData() {
-     const variables = {
-       page: this.infoPage.page,
-       itemsPage: this.infoPage.itemsPage,
-       include: this.include
-     }
-     this.service.getCollectionData(this.query,variables, {}).subscribe((result)=>{
-       console.log(result);
-       
-     })
-   }
+  loadData() {
+    const variables = {
+      page: this.infoPage.page,
+      itemsPage: this.infoPage.itemsPage,
+      include: this.include,
+    };
+    this.data$ = this.service
+      .getCollectionData(this.query, variables, {})
+      .pipe(
+        map((result: any)=> {
+          return result[this.resultData.definitionKey][this.resultData.listKey];
+        })
+      );
+  }
 }
