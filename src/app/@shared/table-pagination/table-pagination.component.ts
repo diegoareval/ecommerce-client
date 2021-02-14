3ca6ef44fce0dcc67aee09f5910fce97ea14e706
Diego <diego2000avelar@gmail.com>
2021-02-14 12:42:59 -0600

    se obtienen los datos desde paginacion service

--- src/app/@shared/table-pagination/table-pagination.component.ts
+++ src/app/@shared/table-pagination/table-pagination.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { USERS_LIST_QUERY } from './../../@graphql/operations/query/user';
+import { TablePaginationService } from './table-pagination.service';
+import { DocumentNode } from 'graphql';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table-pagination',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-pagination.component.scss']
 })
 export class TablePaginationComponent implements OnInit {
-
-  constructor() { }
+   @Input() query : DocumentNode = USERS_LIST_QUERY;
+   @Input() context : object;
+   @Input() itemsPage = 20;
+   @Input() include = true;
+  constructor(private service: TablePaginationService) { }
 
   ngOnInit(): void {
+    if(this.query === undefined){
+       throw new Error("Query is undefined")
+    }
+    this.loadData();
   }
-
+   loadData() {
+     this.service.getCollectionData(this.query, {include: this.include}, {}).subscribe((result)=>{
+       console.log(result);
+       
+     })
+   }
 }
